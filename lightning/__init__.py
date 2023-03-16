da2ec0c7bcaef1b9d1129165d74d84450ddb508b
Francesco Mills <lightsage01@gmail.com>
2023-03-16 09:56:58 -0500

    Update __init__.py

--- lightning/__init__.py
+++ lightning/__init__.py
@@ -6,10 +6,10 @@ This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
 by the Free Software Foundation at version 3 of the License.
 
-This program is distributed in the hope that it will be useful,
+This program is distribute
+d in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 GNU Affero General Public License for more details.
-
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
